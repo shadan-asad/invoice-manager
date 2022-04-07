df5f34580c6d3111d331f5f75beb8d3269135427
shadan-asad <iamshadanasad@gmail.com>
2022-04-07 23:38:27 +0530

    Added invoice adding form

--- src/components/addInvo.js
+++ src/components/addInvo.js
@@ -7,12 +7,21 @@ import TextField from '@mui/material/TextField';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import Grid from '@mui/material/Grid'
 
 
 import '../App.css'
 
 export default function AddInvo({open, handleClose}) {
-  
+  const [clearDate, setClearDate] = React.useState(null);
+  const [postingDate, setPostingDate] = React.useState(null);
+  const [documentCreateDate, setDocumentCreateDate] = React.useState(null);
+  const [dueDate, setDueDate] = React.useState(null);
+  const [baselineCreateDate, setBaselineCreateDate] = React.useState(null);
+
   return (
     <div>
       <Dialog
@@ -25,6 +34,7 @@ export default function AddInvo({open, handleClose}) {
         <DialogTitle id="alert-dialog-title">
           {"Add Invoice"}
         </DialogTitle>
+      <div>
         <DialogContent>
           <Box
             component="form"
@@ -33,9 +43,18 @@ export default function AddInvo({open, handleClose}) {
             }}
             >
             <TextField id="outlined-basic" label="Business Code" variant="outlined" />
-            <TextField id="outlined-basic" label="Customer Number" variant="outlined" />
-            <TextField id="outlined-basic" label="Business Year" variant="outlined" />
-            <TextField id="outlined-basic" label="Business Year" variant="outlined" />
+            <TextField id="outlined-basic" label="Customer Number" variant="outlined" type="number"/>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Clear Date"
+                value={clearDate}
+                onChange={(newValue) => {
+                  setClearDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <TextField id="outlined-basic" label="Business Year" variant="outlined" type="number"/>
           </Box>
           <Box
             component="form"
@@ -44,9 +63,32 @@ export default function AddInvo({open, handleClose}) {
             }}
             >
             <TextField id="outlined-basic" label="Document ID" variant="outlined" />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Posting Date"
+                value={postingDate}
+                onChange={(newValue) => {
+                  setPostingDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <DatePicker
+                label="Document Create Date"
+                value={documentCreateDate}
+                onChange={(newValue) => {
+                  setDocumentCreateDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <DatePicker
+                label="Due Date"
+                value={dueDate}
+                onChange={(newValue) => {
+                  setDueDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
           </Box>
           <Box
             component="form"
@@ -56,8 +98,8 @@ export default function AddInvo({open, handleClose}) {
             >
             <TextField id="outlined-basic" label="Invoice Currency" variant="outlined" />
             <TextField id="outlined-basic" label="Document Type" variant="outlined" />
-            <TextField id="outlined-basic" label="Posting ID" variant="outlined" />
-            <TextField id="outlined-basic" label="Total Open Amount" variant="outlined" />
+            <TextField id="outlined-basic" label="Posting ID" variant="outlined" type="number"/>
+            <TextField id="outlined-basic" label="Total Open Amount" variant="outlined" type="number"/>
           </Box>
           <Box
             component="form"
@@ -65,15 +107,25 @@ export default function AddInvo({open, handleClose}) {
               '& > :not(style)': { m: 1, width: '25ch' },
             }}
             >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Baseline Create Date"
+                value={baselineCreateDate}
+                onChange={(newValue) => {
+                  setBaselineCreateDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
             <TextField id="outlined-basic" label="Customer Payment Terms" variant="outlined" />
-            <TextField id="outlined-basic" label="Invoice ID" variant="outlined" />
+            <TextField id="outlined-basic" label="Invoice ID" variant="outlined" type="number"/>
           </Box>
-          <Stack spacing={2} direction="row">
-            <Button className='button' variant="outlined">Add</Button>
-            <Button className='button' variant="outlined">Cancel</Button>
-          </Stack>
         </DialogContent>
+      </div>
+      <div className='btnGrp'>
+        <Button className='addButton' variant="outlined" onClick={handleClose}>Add</Button>
+        <Button className='addButton' variant="outlined" onClick={handleClose}>Cancel</Button>    
+      </div>
       </Dialog>
     </div>
   );
